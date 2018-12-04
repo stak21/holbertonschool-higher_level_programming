86d6f02aa388692cbc36cb0d3bac4cd29c12770b
stak21 <stakashima21@gmail.com>
2018-12-04 03:40:18 +0000

    A pyscript that prints a string

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,14 +1,2 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
-
-/**
-*
-*
-*/
-
-()
-{
-
-	return (0);
-}
+#!/usr/bin/python3
+print("Programming is like building a multilingual puzzle\n")
