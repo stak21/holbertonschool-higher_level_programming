6367ea075c0fec81e2b0aa98b6098ef83be0db17
stak21 <stakashima21@gmail.com>
2018-12-04 03:45:59 +0000

    a pyscript that prints a string

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,14 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
-
-/**
-*
-*
-*/
-
-()
-{
-
-	return (0);
-}
+#!/usr/bin/python3
+number = 98
+print("{} Battery street".format(number))
