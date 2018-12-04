2df9ffb8953beb1f41f1e917b1d460ba4ca1d260
stak21 <stakashima21@gmail.com>
2018-12-04 04:02:48 +0000

    prints a string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,14 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
-
-/**
-*
-*
-*/
-
-()
-{
-
-	return (0);
-}
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with Python"
+print(str)
