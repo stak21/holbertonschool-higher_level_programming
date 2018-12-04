275022f4a6486864167bff9a69ad20dd4f2ef03c
stak21 <stakashima21@gmail.com>
2018-12-04 03:55:41 +0000

    concat 2 strings and print the result

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,14 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
-
-/**
-*
-*
-*/
-
-()
-{
-
-	return (0);
-}
+#!/usr/bin/python3
+str1= "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
