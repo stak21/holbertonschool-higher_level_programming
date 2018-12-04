b8f3fefe7c1bf3c1a4cf6bc63a60310950770440
stak21 <stakashima21@gmail.com>
2018-12-04 23:56:50 +0000

    prints the alphabet except for q and e, reworked

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(65, 91):
-    char = chr(i).lower()
-    if char != 'q' or char != 'e':
-        print(char, end='')
+    if i != '69' or i != '81':
+        print(chr(i).lower(), end='')
