690fa4f61bfa3a4ec364cb3eaef97fa9a5251661
stak21 <stakashima21@gmail.com>
2018-12-05 02:43:02 +0000

    case fix: negative nuber, 1 node, *head is NULL

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,24 +12,37 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *nxt_ptr;
-	listint_t *prev_ptr;
+	listint_t *prev_ptr = NULL;
 	listint_t *node;
 
 	if (!head)
 		return (NULL);
-	nxt_ptr = (*head)->next;
-	prev_ptr = *head;
-
-	while (nxt_ptr != NULL && nxt_ptr->n <= number)
-	{
-		nxt_ptr = nxt_ptr->next;
-		prev_ptr = prev_ptr->next;
-	}
 	node = malloc(sizeof(listint_t));
 	if (!node)
 		return (NULL);
-	node->next = nxt_ptr;
 	node->n = number;
+	if (!(*head))
+	{
+		node->next = NULL;
+		*head = node;
+		return (node);
+	}
+	if (number < (*head)->n)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
+	nxt_ptr = *head;
+	if (nxt_ptr == NULL)
+
+	while (nxt_ptr != NULL && nxt_ptr->n <= number)
+	{
+		prev_ptr = nxt_ptr;
+		nxt_ptr = nxt_ptr->next;
+	}
+	node->next = nxt_ptr;
 	prev_ptr->next = node;
 
 	return (node);
