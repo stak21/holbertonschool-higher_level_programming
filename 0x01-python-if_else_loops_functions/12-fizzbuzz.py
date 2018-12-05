8fbc0509931aefbf36a2920e321e976798f2168e
stak21 <stakashima21@gmail.com>
2018-12-05 01:53:39 +0000

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-'''
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 is 0:
+            print("fizzbuzz", end='')
+        elif i % 3 is 0:
+            print("fizz", end='')
+        elif i % 5 is 0:
+            print("buzz", end='')
+        else:
+            print("{}".format(i), end='')
+        if i is not 100:
+            print(", ", end='')
