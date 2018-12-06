2f69951839c7182ccfdb228a761b7dd0e0fdd043
stak21 <stakashima21@gmail.com>
2018-12-06 05:48:44 +0000

    import from calc and use its functions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-'''
+from calculator_1.py import add, sub, mul, div
+
+if __name__ == "__main__":
+        import sys
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, sub(a, b)))
+print("{} + {} = {}".format(a, b, mul(a, b)))
+print("{} + {} = {}".format(a, b, div(a, b)))
