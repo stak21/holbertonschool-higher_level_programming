7eb06aba8f61ad302f16db7eefd495bbc074a062
stak21 <stakashima21@gmail.com>
2018-12-06 05:51:27 +0000

    printed the wrong format

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,6 +8,6 @@ a = 10
 b = 5
 
 print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} + {} = {}".format(a, b, sub(a, b)))
-print("{} + {} = {}".format(a, b, mul(a, b)))
-print("{} + {} = {}".format(a, b, div(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
