af1fbb857d7677ab06bef1fe63d56104c03acd2a
stak21 <stakashima21@gmail.com>
2018-12-06 07:18:00 +0000

    import calculator and use it properly

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-'''
+
+import calculator_1
+
+if __name__ == "__main__":
+    import sys
+
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+calc = calculator_1
+func = {'+' : calc.add, '-' : calc.sub, '/' : calc.div, 'x' : calc.mul}
+a = sys.argv[1]
+op = sys.argv[2]
+b = sys.argv[3]
+
+if op in '-+/x':
+    print("{} {} {} = {}".format(a, op, b, func[op](int(a), int(b)))) 
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
