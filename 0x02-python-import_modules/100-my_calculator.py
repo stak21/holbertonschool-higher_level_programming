35a42fad3633a51e078b5b168b37ee3f98f9c686
stak21 <stakashima21@gmail.com>
2018-12-06 07:43:25 +0000

    guarded everything

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,21 @@
 if __name__ == "__main__":
     import sys
 
-import calculator_1
+    import calculator_1
 
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-calc = calculator_1
-func = {'+': calc.add, '-': calc.sub, '/': calc.div, '*': calc.mul}
-a = sys.argv[1]
-op = sys.argv[2]
-b = sys.argv[3]
+    calc = calculator_1
+    func = {'+': calc.add, '-': calc.sub, '/': calc.div, '*': calc.mul}
+    a = sys.argv[1]
+    op = sys.argv[2]
+    b = sys.argv[3]
 
-if op in '-+/*':
-    print("{} {} {} = {}".format(a, op, b, func[op](int(a), int(b))))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    if op in '-+/*':
+        print("{} {} {} = {}".format(a, op, b, func[op](int(a), int(b))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
