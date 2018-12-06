7986a0993975784261ef2e8972b93f670e93c4e4
stak21 <stakashima21@gmail.com>
2018-12-06 06:06:30 +0000

    adding every element in the arg list

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-'''
+
+if __name__ == "__main__":
+    import sys
+
+result = 0
+for arg in sys.argv:
+    if arg != sys.argv[0]:
+        result += int(arg)
+
+print("{}".format(result))
