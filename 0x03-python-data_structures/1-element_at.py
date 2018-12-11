a74e5d6c5b898758820f55b4569bbf756d40eefd
stak21 <stakashima21@gmail.com>
2018-12-11 20:59:10 +0000

    check if theres a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None 
-    if idx > len(my_list):
+    if len(my_list) is 0:
         return None
-
+    if idx < 0 or idx > len(my_list):
+        return None
     return (my_list[idx])
