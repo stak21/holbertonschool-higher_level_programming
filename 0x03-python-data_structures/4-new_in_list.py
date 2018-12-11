9e04af2e7a09f3447ed231e5a4e719deae3027ad
stak21 <stakashima21@gmail.com>
2018-12-11 21:45:24 +0000

    return value changed form none to a copy of the list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if not isinstance(my_list, list):
-        return None
+        return my_list.copy()
     if idx < 0 or idx >= len(my_list):
-        return None
+        return my_list.copy()
     copy = my_list.copy()
     copy[idx] = element
     return copy
