fc5d009d99d3ccc70f1127aa92c48c69de80c3dc
stak21 <stakashima21@gmail.com>
2018-12-11 21:05:19 +0000

    using isinstance

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if not my_list:
+    if not isinstance(my_list, list):
         return None
     if idx < 0 or idx > len(my_list):
         return None
