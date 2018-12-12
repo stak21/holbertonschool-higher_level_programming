a7a9053ed0d49aaa494724ab7aafe12a7177f166
stak21 <stakashima21@gmail.com>
2018-12-12 01:40:28 +0000

    initial commit

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tup_ret = ()
+    count = 0
+
+    for i, j in zip(tuple_a, tuple_b):
+        if count == 2:
+            break
+        if not i:
+            i = 0
+        if not j:
+            j = 0
+        count += 1
+        tup_ret += ((i + j),)
+    if count == 1:
+        if tuple_a[1]:
+            tup_ret += (tuple_a[1],)
+        else:
+            tuple_ret += (tuple_b[1],)
+    if count == 0:
+        if tuple_a[0]:
+            return tuple_a
+        else:
+            return tuple_b
+    return tup_ret
