1b01cb865a94ffa27992e69dca9379eb8c3e1e20
stak21 <stakashima21@gmail.com>
2018-12-12 01:43:54 +0000

    case: check for 0

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,12 +13,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         count += 1
         tup_ret += ((i + j),)
     if count == 1:
-        if tuple_a[1]:
+        if tuple_a[1] >= 0:
             tup_ret += (tuple_a[1],)
         else:
             tuple_ret += (tuple_b[1],)
     if count == 0:
-        if tuple_a[0]:
+        if tuple_a[0] >= 0:
             return tuple_a
         else:
             return tuple_b
