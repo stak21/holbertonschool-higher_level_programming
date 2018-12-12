7a665a9b229cf6332102c74cee94a0b6aca184ea
stak21 <stakashima21@gmail.com>
2018-12-12 03:23:29 +0000

    replaced code with try and except

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tup_ret = ()
-    count = 0
+    a1 = a2 = b1 = b2 = 0
 
-    for i, j in zip(tuple_a, tuple_b):
-        if count == 2:
-            break
-        if not i:
-            i = 0
-        if not j:
-            j = 0
-        count += 1
-        tup_ret += ((i + j),)
-    if count == 1:
-        if tuple_a[1] >= 0:
-            tup_ret += (tuple_a[1],)
-        else:
-            tuple_ret += (tuple_b[1],)
-    if count == 0:
-        if tuple_a[0] >= 0:
-            return tuple_a
-        else:
-            return tuple_b
-    return tup_ret
+    try:
+        a1 = tuple_a[0]
+    except:
+        a1 = 0
+    try:
+        b1 = tuple_b[0]
+    except:
+        b1 = 0
+    try:
+        a2 = tuple_a[1]
+    except:
+        a2 = 0
+    try:
+        b2 = tuple_b[1]
+    except:
+        b2 = 0
+    return ((a1 + b1), (a2 + b2))
