9278cc86571f5989be42deb55f72ccc235b945a2
stak21 <stakashima21@gmail.com>
2018-12-12 01:55:14 +0000

    checked for negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    biggest = 0
+    biggest = None
     for i in my_list:
+        if biggest == None:
+            biggest = i
         if i > biggest:
             biggest = i
-
     return biggest
