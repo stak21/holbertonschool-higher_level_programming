37606a28a1edaf53e13037b5bec90b48040fe034
stak21 <stakashima21@gmail.com>
2018-12-12 02:11:29 +0000

    initial commit

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if not isinstance(my_list, list):
+        return None
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    newlist = []
+    for i in my_list:
+        if my_list[idx] != i:
+            newlist.append(i)
+    return newlist
