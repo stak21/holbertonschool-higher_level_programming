4b01f3088ebf43b4783c27b2e9eb0f67b169f353
stak21 <stakashima21@gmail.com>
2018-12-13 23:06:38 +0000

    check if theres an empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     return max(a_dictionary.keys())
