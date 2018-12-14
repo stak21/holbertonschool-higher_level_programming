8df33304e472001b04952c4b1f286b6ca0b26066
stak21 <stakashima21@gmail.com>
2018-12-14 00:36:21 +0000

    change roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    numeral = {'I': 1, 'V': 5, 'X': 10, 'IV': -1, 'IX': -1, 'L': 50, 'C': 100,
+               'D': 500, 'M': 1000}
+    num = []
+    roman_string += 'z'
+    for first, second in zip(roman_string, roman_string[1:]):
+        try:
+            num.append(numeral[first + second])
+        except:
+            num.append(numeral.get(first))
+    return sum(num)
