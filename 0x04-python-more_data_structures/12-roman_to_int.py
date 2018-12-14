676fa8f4de14290f5180779dbc0a6f1f6bcbfbd3
stak21 <stakashima21@gmail.com>
2018-12-14 02:52:09 +0000

    pep8 compliant

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     numeral = {'I': 1, 'V': 5, 'X': 10, 'IV': -1, 'IX': -1, 'L': 50, 'C': 100,
-               'D': 500, 'M': 1000, 'CM':-100, 'XC': -10}
+               'D': 500, 'M': 1000, 'CM': -100, 'XC': -10}
     num = []
     roman_string += 'z'
     for first, second in zip(roman_string, roman_string[1:]):
