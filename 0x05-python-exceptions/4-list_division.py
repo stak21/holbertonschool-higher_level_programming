dd3b8c5a57e03219ec067d2a3842bffc06a94970
stak21 <stakashima21@gmail.com>
2019-01-02 23:21:07 +0000

    pep8 compliant

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,5 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-        newlist.append(res)
+            newlist.append(res)
     return newlist
