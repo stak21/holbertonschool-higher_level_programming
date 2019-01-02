b068fc47d37e61d5505edf925897d03c34279b27
stak21 <stakashima21@gmail.com>
2019-01-02 22:21:52 +0000

    returning a count instead of idx

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
+
     try:
         for idx in range(x):
             print("{:d}".format(my_list[idx]), end="")
+            count += 1
     except IndexError:
         print("")
-        return idx
+        return count
     else:
         print("")
-        return idx
+        return count
