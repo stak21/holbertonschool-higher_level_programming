0771abc88b21af8a43a2bbd3dfaf8b0852275108
stak21 <stakashima21@gmail.com>
2019-01-02 23:58:21 +0000

    forgot to print error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
     except (TypeError, ValueError) as ve:
         tmp = ve.args
-        sys.stderr.write(tmp[0])
+        sys.stderr.write("Exception: {}".format(tmp[0]))
         return False
     else:
         return True
