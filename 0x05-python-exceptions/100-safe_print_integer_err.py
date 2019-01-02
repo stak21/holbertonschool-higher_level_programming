d5589987da35606f937354fe9d9cef8b03ec2ac1
stak21 <stakashima21@gmail.com>
2019-01-02 23:55:37 +0000

    added an exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
-    except ValueError as ve:
+    except (TypeError, ValueError) as ve:
         tmp = ve.args
         sys.stderr.write(tmp[0])
         return False
