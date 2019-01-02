b52690f44017955d083bc0a43303e112d349b9d8
stak21 <stakashima21@gmail.com>
2019-01-02 23:44:32 +0000

    initial commit: Write a function that prints an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except ValueError as ve:
+        tmp = ve.args
+        sys.stderr.write(tmp[0])
+        return False
+    else:
+        return True
