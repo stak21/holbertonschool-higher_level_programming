3126c284b9dbae93930ca66a9a76e3fed5604682
stak21 <stakashima21@gmail.com>
2019-01-02 22:18:03 +0000

    change exception to look for IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for idx in range(x):
             print("{:d}".format(my_list[idx]), end="")
-    except:
+    except IndexError:
         print("")
         return idx
     else:
