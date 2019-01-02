8572f4ac0553362578b30ce8888a99ae80b2bb11
stak21 <stakashima21@gmail.com>
2019-01-02 23:18:04 +0000

    initial commit: divides elements in 2 lists and returns the new list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    newlist = []
+    for idx in range(list_length):
+        res = 0
+        try:
+            res = my_list_1[idx] / my_list_2[idx]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+
+        newlist.append(res)
+    return newlist
+        
