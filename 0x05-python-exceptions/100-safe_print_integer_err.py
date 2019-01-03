e93cf60a747dc2150b287865d8950bed5555b826
stak21 <stakashima21@gmail.com>
2019-01-03 00:02:09 +0000

    moved return value frmo else to try

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return True
     except (TypeError, ValueError) as ve:
         sys.stderr.write("Exception: {}".format(ve.args[0]))
         return False
-    else:
-        return True
