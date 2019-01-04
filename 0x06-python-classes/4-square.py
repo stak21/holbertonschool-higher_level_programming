bdfc36c474dc4e3dd4977c3c9eee5b12ceba0069
stak21 <stakashima21@gmail.com>
2019-01-04 01:37:03 +0000

    initial commit: use properties to set and get values

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create a Square class"""
+
+
+class Square:
+    """Add to Square- property setter"""
+    def __init__(self, size=0):
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(self.size) is not int:
+            raise TypeError("size must be an integer")
+        if self.size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.size ** 2
