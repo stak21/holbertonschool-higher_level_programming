74e892ea3fd0ffd0c09671ac8a3ca38f5632872f
stak21 <stakashima21@gmail.com>
2019-01-04 01:30:41 +0000

    initial commit: Addition to Square- create a instance method area()

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Create a Square class"""
+
+class Square:
+    """This new Square creates a instance method"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size ** 2
