39d69da98c5cd5de04317281eb724b2f056874d2
stak21 <stakashima21@gmail.com>
2019-01-04 01:24:10 +0000

    initial commit: Create a Square class with safe initialization

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A Square class"""
+
+
+class Square:
+    """A simple Square with a size"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+
