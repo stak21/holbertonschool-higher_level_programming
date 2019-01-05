d0171adbe09a01bb81a2cd9fe2df5c2440a52c20
stak21 <stakashima21@gmail.com>
2019-01-05 19:04:55 +0000

    initial commit: ByteCode!

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a MagicClass that does exactly as a given bytecode"""
+import math
+
+
+class MagicClass:
+    """lets find out"""
+    def __init__(self, radius):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
