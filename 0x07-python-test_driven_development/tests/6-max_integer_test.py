02a2c1118dc9918e28f105a10c962f83b522f7a9
stak21 <stakashima21@gmail.com>
2019-01-09 21:54:51 +0000

    Wrote several tests: negative numbers, empty list, max in middle, max in beginning, max in end

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -25,4 +25,30 @@ class TestMaxInteger(unittest.TestCase):
         """ Test a list of strings """
         self.assertEqual(max_integer(['hi', 'bye']), 'hi')
 
+    def testMaxAtEnd(self):
+        """ Tests max number at the end of a list """
+        self.assertEqual(max_integer([3, 2, 4]), 4)
 
+    def testMaxAtBeginning(self):
+        """ Tests max number at the beginning of a list """
+        self.assertEqual(max_integer([5, 2, 3]), 5)
+
+    def testNegative(self):
+        """ Tests using a negative number """
+        self.assertEqual(max_integer([-1, 2]), 2)
+
+    def testTwoNegative(self):
+        """ Tests using both parameters as negative """
+        self.assertEqual(max_integer([-1, -4]), -1)
+
+    def testMaxMiddle(self):
+        """ Tests max number in the middle """
+        self.assertEqual(max_integer([1,5,2]), 5)
+
+    def testOneNum(self):
+        """ Tests one number in the list """
+        self.assertEqual(max_integer([1]), 1)
+
+    def testEmpty(self):
+        """ Tests an empty list """
+        self.assertEqual(max_integer([]), None)
