33bac08b86dc459d25ce1f5b59965d8083a13e4d
stak21 <stakashima21@gmail.com>
2019-01-10 17:13:54 +0000

    set default values

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = '#'
 
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         """ initiates fields """
         self.height = height
         self.width = width
