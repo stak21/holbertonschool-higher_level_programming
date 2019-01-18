5a05dc871680c449df4aec488f74af887d4cd5a5
stak21 <stakashima21@gmail.com>
2019-01-18 05:50:53 +0000

    Initial commit: Read from a JSON file and append to the object

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+""" Script: adds all arguments to a Python list and save them to a file """
+
+filename = "add_item.json"
+existing_li = load_from_json_file(filename)
+with open(filename, "w") as f:
+    for args in sys.argv[1:]:
+        if existing_li is None:
+            existing_li = []
+        existing_li.append(args)
+    save_to_json_file(existing_li, filename)
