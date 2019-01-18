b9b3455c6b31f270fbc7295d01bb551a4fd166ed
stak21 <stakashima21@gmail.com>
2019-01-18 02:52:37 +0000

    removed testcode

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,17 +7,7 @@ def read_lines(filename="", nb_lines=0):
     count = 0
     with open(filename, encoding="utf-8") as f:
         for line in f:
-            if nb_lines is not 0 and count == nb_lines:
+            if nb_lines != 0 and count == nb_lines:
                 break
             print(line, end="")
             count += 1
-
-
-print("1 line:")
-read_lines("my_file_0.txt", 1)
-print("--")
-print("3 lines:")
-read_lines("my_file_0.txt", 3)
-print("--")
-print("Full content:")
-read_lines("my_file_0.txt")
