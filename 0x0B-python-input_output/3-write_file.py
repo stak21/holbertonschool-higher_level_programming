12094a4937ad223b4ef368aa60fce0ff06cffc26
stak21 <stakashima21@gmail.com>
2019-01-18 02:57:22 +0000

    initial commit: write/overwrite to a file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Function: write_file """
+
+
+def write_file(filename="", text=""):
+    """ Write a string to a text file and return the number of characters
+    written """
+    with open(filename,"w", encoding="utf-8") as f:
+        count = f.write(text)
+        return count
