a67c61f0da0c0d07e6b19c639865d1d8ac5cef9c
stak21 <stakashima21@gmail.com>
2019-01-18 04:06:00 +0000

    Initial commit: return a encoded object created from JSON

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" Function: from_json_string """
+
+
+def from_json_string(my_str):
+    """ Return an object represented by JSON string """
+    return json.loads(my_str, Sort_)
