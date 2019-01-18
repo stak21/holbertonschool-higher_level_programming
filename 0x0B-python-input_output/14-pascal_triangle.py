775ba380e6cdac301d8528a4159d5f9ba8679029
stak21 <stakashima21@gmail.com>
2019-01-18 08:43:34 +0000

    Initial commit: Pascal

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Function: pascal_triangle """
+
+
+def pascal_triangle(n):
+    """ return a list of lists of integers representing Pascal's triangle """
+    li = [[1]]
+    if n <= 0:
+        return li
+    for row in range(1, n):
+        li.append([1])
+        for count in range(row):
+            try:
+                num = li[row - 1][count + 1] + li[row - 1][count]
+            except:
+                num = 1
+            if row - 1 == count:
+                num = 1
+            li[row].append(num)
