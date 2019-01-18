d6b74f42d0ff2be72dde8e3cd881185c01603880
stak21 <stakashima21@gmail.com>
2019-01-18 04:13:13 +0000

    Initial commit: Write an object representation to a file

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+""" Function: save_to_json_file """
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes an Object to a file, using JSON representation """
+    with open(filename, "w", encoding="utf_8") as f:
+        f.write(json.dumps(my_obj))
