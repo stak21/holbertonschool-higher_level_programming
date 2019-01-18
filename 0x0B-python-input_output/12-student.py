81b8ead7d7fb0905ad3307c743931b95ee897f2f
stak21 <stakashima21@gmail.com>
2019-01-18 07:36:24 +0000

    Initial commit: Class - extend attrs to to_json

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student:
+    """ Extend: attrs """
+    def __init__(self, first_name, last_name, age):
+        """ instantiate first_name, last_name, age """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
