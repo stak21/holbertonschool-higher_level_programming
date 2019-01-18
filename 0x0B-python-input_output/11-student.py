e75b518879359231609aaabd0e5e5eef1699ac04
stak21 <stakashima21@gmail.com>
2019-01-18 07:25:25 +0000

    Removed decoratores for setter and getter

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,36 +10,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    @property
-    def first_name(self):
-        """ return first_name """
-        return self.__first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        """ Set a private first_name """
-        self.__first_name = value
-
-    @property
-    def last_name(self):
-        """ return last_name """
-        return self.__last_name
-
-    @last_name.setter
-    def last_name(self, value):
-        """ Set a private last_name """
-        self.__last_name = value
-
-    @property
-    def age_name(self):
-        """ return age_name """
-        return self.__age_name
-
-    @age_name.setter
-    def age_name(self, value):
-        """ Set a private age_name """
-        self.__age_name = value
-
     def to_json(self):
         """ Retrieves a dictionary representation of a Student instance """
         return self.__dict__
