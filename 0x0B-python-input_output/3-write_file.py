411725a04feac10cabd702857774e8b97c041c56
stak21 <stakashima21@gmail.com>
2019-01-18 03:03:02 +0000

    pep8 compliant

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -3,8 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """ Write a string to a text file and return the number of characters
-    written """
+    """ Write a string to a file and return the number of characters """
     with open(filename,"w", encoding="utf-8") as f:
         count = f.write(text)
         return count
