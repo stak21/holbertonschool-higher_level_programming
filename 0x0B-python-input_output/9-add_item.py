3058d8f4b717320c05441a5bc0cb0fbcc8cb3d62
stak21 <stakashima21@gmail.com>
2019-01-18 06:32:27 +0000

    created an empty file if it doesn't exist

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,8 +5,10 @@ save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 """ Script: adds all arguments to a Python list and save them to a file """
 
 filename = "add_item.json"
+with open(filename, 'a', encoding="utf-8") as f:
+    pass
 existing_li = load_from_json_file(filename)
-with open(filename, "w") as f:
+with open(filename, "w+") as f:
     for args in sys.argv[1:]:
         if existing_li is None:
             existing_li = []
