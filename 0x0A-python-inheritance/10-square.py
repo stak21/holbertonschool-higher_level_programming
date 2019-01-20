14f4241c0d504f6e86b77f7669860d54684d723b
stak21 <stakashima21@gmail.com>
2019-01-20 07:57:15 +0000

    Initial Commit: SubClass- Square of Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+""" Class Square - inherits from Rectangle """
+
+
+class Square(Rectangle):
+    """ implement init and area """
+    def __init__(self, size):
+        """ instantiate size """
+        super().__init__(size, size)
+        
+    def area(self):
+        """ Return the area of a square """
+        super().area()
