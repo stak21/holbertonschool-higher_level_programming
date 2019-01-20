1ca003a0525e649dd19622d4c605085a042c5b19
stak21 <stakashima21@gmail.com>
2019-01-20 08:53:07 +0000

    Added method(__str__)

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+""" Class Square - inherits from Rectangle """
+
+
+class Square(Rectangle):
+    """ implement init and area """
+    def __init__(self, size):
+        """ instantiate size """
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
+
+    def area(self):
+        """ Return the area of a square """
+        return super().area()
+
+s = Square(5)
+print(s)
