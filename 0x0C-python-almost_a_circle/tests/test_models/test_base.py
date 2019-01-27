8d768fd173bdf010f7cf083e332f3e995c64cdc3
stak21 <stakashima21@gmail.com>
2019-01-27 22:01:43 +0000

    Imported Rectangle && Square ; copied over all json related tests from rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 import os
 import unittest
+
 
 class TestBase(unittest.TestCase):
     """ Testing implementation of a class: Base """
     def setUp(self):
         Base._reset()
 
-    def test_normal(self): 
+    def test_normal(self):
         self.base = Base(5)
         self.assertEqual(self.base.id, 5)
         """ Check if nb_objects incremented """
@@ -37,11 +40,11 @@ class TestBase(unittest.TestCase):
             self.base = Base({2: 5})
         with self.assertRaises(TypeError):
             self.base = Base((1,))
-            
+
     def test_bad_value(self):
         with self.assertRaises(ValueError):
             self.base = Base(-4)
-            
+
     def test_arg_count(self):
         with self.assertRaises(TypeError):
             self.base = Base(2, 2)
@@ -63,3 +66,42 @@ class TestBase(unittest.TestCase):
         self.assertEqual(json_dict, "[]")
         json_dict = self.base.to_json_string(None)
         self.assertEqual(json_dict, "[]")
+
+    def test_json_to_file(self):
+        """ Test save_json: note* 52 characters per dict """
+        self.r1 = Rectangle(1, 2)
+        Rectangle.save_to_file([self.r1])
+        with open("Rectangle.json", "r") as f:
+            data = f.read()
+            self.assertEqual(len(data), 52)
+        self.r2 = Rectangle(3, 4)
+        Rectangle.save_to_file([self.r1, self.r2])
+        with open("Rectangle.json", "r") as f:
+            data = f.read()
+            self.assertEqual(len(data), 104)
+        """ Test empty list """
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as f:
+            data = f.read()
+            self.assertEqual(len(data), 2)
+
+    def test_from_json(self):
+        self.rec = Rectangle(1, 2)
+        li = [self.rec.to_dictionary()]
+        json_list_input = Rectangle.to_json_string(li)
+        from_json = Rectangle.from_json_string(json_list_input)
+        self.assertTrue(isinstance(from_json, list))
+        self.assertTrue(isinstance(from_json[0], dict))
+        """ Test empty or None """
+        self.assertEqual(Rectangle.from_json_string(None), [])
+        self.assertEqual(Rectangle.from_json_string("[]"), [])
+
+    def test_create_cls(self):
+        """ Test the function create: returns an instance with all attributes
+        set """
+        self.r1 = Rectangle(10, 10, 10)
+        self.r1_dict = self.r1.to_dictionary()
+        self.r2 = Rectangle.create(**self.r1_dict)
+        self.assertEqual(self.r2.__str__(), "[Rectangle] (1) 10/0 - 10/10")
+        self.assertTrue(self.r2 is not self.r1)
+        self.assertTrue(self.r2 != self.r1)
