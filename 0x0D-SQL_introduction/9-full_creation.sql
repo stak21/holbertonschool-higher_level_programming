e3cfc0eb3414edc44c4a6dc8558dcc49b730c914
stak21 <stakashima21@gmail.com>
2019-03-05 05:17:51 +0000

    Initial Commit: Create a Table and adds three rows of information

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Create a second_table with multiple rows
+-- id INT, name VARCHAR(256), score INT
+-- if it exists don't do anything
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
