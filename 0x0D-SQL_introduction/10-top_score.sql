544e662b021b39daf7044088379d78704da75e9d
stak21 <stakashima21@gmail.com>
2019-03-05 05:27:20 +0000

    Initial Commit: Select specific keys from a table and order them

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table second_table
+-- Results should display the score and the name
+-- Records should be ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
