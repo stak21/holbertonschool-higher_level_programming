008c0cb4b90bc244d14dfe24b991aeecb51c1ae1
stak21 <stakashima21@gmail.com>
2019-03-06 02:59:41 +0000

    Initial Commit: Create a DB and user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates the DB hbtn_0d_2 and the user user_0d_2
+-- user should have SELECT in DB hbtn_0d_2; password: user_0d_2_pwd
+-- if it exists it shouldn't fail
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost IDENTiFIED BY 'user_0d_2_pwd';
+
