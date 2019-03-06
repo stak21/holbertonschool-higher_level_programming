172f2885dfb377a0e17e5fdd673b2526eb89bea2
stak21 <stakashima21@gmail.com>
2019-03-06 09:30:15 +0000

    removed AS

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 -- list all genres of the show Dexter
 -- display tv_genres.name
 -- Results: ASC by genre name
-SELECT tv_genres.name as name 
+SELECT tv_genres.name
 	FROM tv_shows 
 	JOIN tv_show_genres 
 	ON tv_shows.title = 'Dexter' AND tv_shows.id = tv_show_genres.show_id 
 	INNER JOIN tv_genres 
 	ON tv_genres.id = tv_show_genres.genre_id 
-	ORDER BY tv_genres.name ASC;
+	ORDER BY tv_genres.name;
