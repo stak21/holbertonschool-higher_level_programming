21a67be9fe697a2f3cfa9b7a7d99de9f48ed8671
stak21 <stakashima21@gmail.com>
2019-03-06 02:55:28 +0000

    Initial Commit: Creates a user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates user_0d_1
+-- Privileges: all privileges; password: user_0d_1_pwd
+-- if user exists it should not fail
+GRANT ALL on *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
