ab348baeb3c972fe26f62849aa8aa51239114ded
stak21 <stakashima21@gmail.com>
2019-03-06 03:12:31 +0000

    Addition: IF NOT EXISTS

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates the DB hbtn_0d_2 and the user user_0d_2
 -- user should have SELECT in DB hbtn_0d_2; password: user_0d_2_pwd
 -- if it exists it shouldn't fail
-CREATE DATABASE hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost IDENTiFIED BY 'user_0d_2_pwd';
+CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
