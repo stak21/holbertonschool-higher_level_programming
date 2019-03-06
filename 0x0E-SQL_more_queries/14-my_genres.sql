4b80a070b3d9c74e51f1aa6954a65fa5c4ead111
stak21 <stakashima21@gmail.com>
2019-03-06 09:29:07 +0000

    Removed the second select

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,14 +1,10 @@
 -- list all genres of the show Dexter
 -- display tv_genres.name
 -- Results: ASC by genre name
-SELECT DISTINCT tv_genres.name as name
-	FROM tv_show_genres 
-	INNER JOIN tv_shows 
-	ON tv_show_genres.show_id = (
-		SELECT tv_shows.id 
-		FROM tv_shows 
-		WHERE tv_shows.title = 'Dexter') 
-	RIGHT JOIN tv_genres 
+SELECT tv_genres.name as name 
+	FROM tv_shows 
+	JOIN tv_show_genres 
+	ON tv_shows.title = 'Dexter' AND tv_shows.id = tv_show_genres.show_id 
+	INNER JOIN tv_genres 
 	ON tv_genres.id = tv_show_genres.genre_id 
-	WHERE tv_show_genres.genre_id IS NOT NULL 
 	ORDER BY tv_genres.name ASC;
