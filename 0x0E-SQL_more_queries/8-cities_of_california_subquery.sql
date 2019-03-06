d5f455b0a4a84ec7339acb601aa2e125aa0ea48c
stak21 <stakashima21@gmail.com>
2019-03-06 03:56:47 +0000

    Addition ORDER BY ID

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- states contains record where name = California
 -- Results ascd order by cities.id
 -- no join
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
