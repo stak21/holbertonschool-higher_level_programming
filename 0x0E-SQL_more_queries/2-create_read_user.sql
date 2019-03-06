6f8caf21ea91cd09d42235f9b1f76a11674d099b
stak21 <stakashima21@gmail.com>
2019-03-06 03:13:42 +0000

    Wrong place: moved the statement IF NOT EXISTS

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creates the DB hbtn_0d_2 and the user user_0d_2
 -- user should have SELECT in DB hbtn_0d_2; password: user_0d_2_pwd
 -- if it exists it shouldn't fail
-CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
