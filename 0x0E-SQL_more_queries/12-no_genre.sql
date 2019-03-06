094242cf6e2d45327955943348713212eef46e91
stak21 <stakashima21@gmail.com>
2019-03-06 05:24:26 +0000

    Initial Commit: List shows without a genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- list all shows without a genre
+-- display: tv_shows.title - tv_show_genres.genre_id
+-- Order by tv_shows.title and tv_show_genres.genre_id asc
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+	FROM tv_shows 
+	LEFT JOIN tv_show_genres 
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_show_genres.show_id IS NULL
+	ORDER BY tv_shows.title, tv_show_genres.genre_id;
