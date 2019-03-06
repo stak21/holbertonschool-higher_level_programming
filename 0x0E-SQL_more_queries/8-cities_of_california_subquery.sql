8dbfd3c14e4c03843cf5c457459f07fa6cecf7b4
stak21 <stakashima21@gmail.com>
2019-03-06 03:54:02 +0000

    Initial Commit: List all cities form a state

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Lists all the cities of CA that can be found in the DB hbtn_0d_usa
+-- states contains record where name = California
+-- Results ascd order by cities.id
+-- no join
+SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
