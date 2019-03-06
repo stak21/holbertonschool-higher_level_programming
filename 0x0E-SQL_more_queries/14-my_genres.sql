7c957f88a1ef3233acd69ca0c17df1cdeacd50ab
stak21 <stakashima21@gmail.com>
2019-03-06 08:46:40 +0000

    Initial Commit: Display the genres of a tvshow

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,14 @@
+-- list all genres of the show Dexter
+-- display tv_genres.name
+-- Results: ASC by genre name
+SELECT DISTINCT tv_genres.name 
+	FROM tv_show_genres 
+	INNER JOIN tv_shows 
+	ON tv_show_genres.show_id = (
+		SELECT tv_shows.id 
+		FROM tv_shows 
+		WHERE tv_shows.title = 'Dexter') 
+	RIGHT JOIN tv_genres 
+	ON tv_genres.id = tv_show_genres.genre_id 
+	WHERE tv_show_genres.genre_id IS NOT NULL 
+	ORDER BY tv_genres.name;
