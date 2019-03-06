e8224bdb476f7946deca58c8f35d6ec4706916dc
stak21 <stakashima21@gmail.com>
2019-03-06 08:48:26 +0000

    Addition: ACS order by

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- list all genres of the show Dexter
 -- display tv_genres.name
 -- Results: ASC by genre name
-SELECT DISTINCT tv_genres.name 
+SELECT DISTINCT tv_genres.name as name
 	FROM tv_show_genres 
 	INNER JOIN tv_shows 
 	ON tv_show_genres.show_id = (
@@ -11,4 +11,4 @@ SELECT DISTINCT tv_genres.name
 	RIGHT JOIN tv_genres 
 	ON tv_genres.id = tv_show_genres.genre_id 
 	WHERE tv_show_genres.genre_id IS NOT NULL 
-	ORDER BY tv_genres.name;
+	ORDER BY tv_genres.name ASC;
