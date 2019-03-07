c673e245d2eefdd9242804bf52df2c40e1ccb4e3
stak21 <stakashima21@gmail.com>
2019-03-07 01:36:01 +0000

    Initial Commit: list all genres by rating

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,11 @@
+-- List al genres by their rating
+-- Display tv_genres.name - rating sum
+-- DESC order by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate)
+	FROM tv_show_ratings
+	JOIN tv_show_genres
+	ON tv_show_ratings.show_id = tv_show_genres.show_id
+	JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	GROUP BY tv_genres.name
+	ORDER BY SUM(tv_show_ratings.rate) DESC;
