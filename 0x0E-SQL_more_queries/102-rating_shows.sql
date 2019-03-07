194e3bf6232d4cf746e0367f56b080003db7dc72
stak21 <stakashima21@gmail.com>
2019-03-07 01:18:26 +0000

    Changed rating name

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,8 @@
 -- List all shows by their rating
 -- Display tv_shows.title - rating sum
 -- DESC order by rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) FROM tv_show_ratings
+SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rating
+	FROM tv_show_ratings
 	JOIN tv_shows
 	ON tv_shows.id = tv_show_ratings.show_id
 	GROUP BY tv_show_ratings.show_id
