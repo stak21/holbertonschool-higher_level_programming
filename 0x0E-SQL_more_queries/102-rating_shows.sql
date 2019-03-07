1b6fbb4ceca3211dce5ceedc74ae61b5d4015643
stak21 <stakashima21@gmail.com>
2019-03-07 01:17:20 +0000

    Initial Commit: List all shows by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- List all shows by their rating
+-- Display tv_shows.title - rating sum
+-- DESC order by rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) FROM tv_show_ratings
+	JOIN tv_shows
+	ON tv_shows.id = tv_show_ratings.show_id
+	GROUP BY tv_show_ratings.show_id
+	ORDER BY SUM(tv_show_ratings.rate) DESC;
