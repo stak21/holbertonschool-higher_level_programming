4ee3c0b381b52ed6f05ac5ef248868b299754071
stak21 <stakashima21@gmail.com>
2019-03-29 23:14:30 +0000

    Added what id to order by

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,11 @@ if __name__ == "__main__":
                 charset="utf8")
         cur = conn.cursor()
         cur.execute(
-                "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+                "SELECT *\
+                        FROM states\
+                        WHERE name\
+                        LIKE 'N%'\
+                        ORDER BY states.id ASC")
         query_rows = cur.fetchall()
         for row in query_rows:
             print(row)
