8ba40d7b3d27890f2951fc799ff40cdb01e357c5
stak21 <stakashima21@gmail.com>
2019-03-29 23:40:59 +0000

    Pep8 Compliant, Binary case sensitivity problem with mysql

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,11 +5,16 @@ import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) == 5:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+        conn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                charset="utf8")
         cur = conn.cursor()
-        cur.execute(\
-                "SELECT * FROM states WHERE name LIKE '{}'\
+        cur.execute(
+                "SELECT * FROM states WHERE BINARY name LIKE '{}'\
                 ORDER BY id ASC".format(sys.argv[4]))
         query_rows = cur.fetchall()
         for row in query_rows:
@@ -18,4 +23,3 @@ if __name__ == "__main__":
         conn.close()
     else:
         print("Usage: USER PASSWORD DB_NAME SEARCH")
-
