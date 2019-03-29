dc6a57feda9d1cd7096ef365bcad14fed374be0d
stak21 <stakashima21@gmail.com>
2019-03-29 23:01:12 +0000

    pep8 compliant

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,14 @@ import MySQLdb
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: USER, PASSWD, DB_NAME")
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+        return None
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
