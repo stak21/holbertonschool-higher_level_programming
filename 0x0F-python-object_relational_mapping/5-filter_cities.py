9fd148de652bd03e49473879c697c6d1134ec692
stak21 <stakashima21@gmail.com>
2019-03-29 04:41:09 +0000

    Initial Commit: Lists all cities filtered by a given argument

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script: lists all cities  """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        state_name = sys.argv[4]
+        cmd = "SELECT cities.name\
+                FROM states \
+                JOIN cities on cities.state_id = states.id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC;"
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+        cur = conn.cursor()
+        cur.execute(cmd, (state_name,))
+        query_rows = cur.fetchall()
+        for row in query_rows[0:-1]:
+            print(row[0], end=', ')
+        print(query_rows[-1][0])
+        cur.close()
+        conn.close()
+    else:
+        print("Usage: USER, PASSWD, DB_NAME")
