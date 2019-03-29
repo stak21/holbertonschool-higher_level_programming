9a486f7680a5949da8edac1753586b3cd577ec11
stak21 <stakashima21@gmail.com>
2019-03-29 04:05:11 +0000

    List all cities with their corresponding states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Script: lists all cities  """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        cmd = "SELECT cities.id, cities.name, states.name\
+                FROM states \
+                JOIN cities on cities.state_id = states.id;"
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+        cur = conn.cursor()
+        cur.execute(cmd)
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+    else:
+        print("Usage: USER, PASSWD, DB_NAME")
