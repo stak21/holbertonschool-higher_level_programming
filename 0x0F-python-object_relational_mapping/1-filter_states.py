8390bf16e5c2a5573bad79038c274d616f2eada0
stak21 <stakashima21@gmail.com>
2019-03-29 23:37:32 +0000

    Mysql is not case sensitive, so we need to check byte by byte using BINARY

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,22 +4,25 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            charset="utf8")
-    cur = conn.cursor()
-    cur.execute(
-            "SELECT *\
-                    FROM states\
-                    WHERE states.name\
-                    LIKE 'N%'\
-                    ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    if len(sys.argv) == 4:
+        conn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                charset="utf8")
+        cur = conn.cursor()
+        cur.execute(
+                "SELECT *\
+                        FROM states\
+                        WHERE BINARY states.name\
+                        LIKE 'N%'\
+                        ORDER BY states.id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+    else:
+        print("Usage: USER, PASSWD, DB_NAME")
