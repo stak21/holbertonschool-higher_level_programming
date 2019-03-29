bc8c51298a526ce1bae22d3309edad4dbfd001de
stak21 <stakashima21@gmail.com>
2019-03-29 04:02:49 +0000

    Lists all states with a name N

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Script: lists all states from the db """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: USER, PASSWD, DB_NAME")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
