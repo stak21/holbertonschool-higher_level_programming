86fa43b42153e4fcc2f769bb9ba6ff92331b7757
stak21 <stakashima21@gmail.com>
2019-03-29 23:47:54 +0000

    Pep8 Compliant

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,8 +11,13 @@ if __name__ == "__main__":
                 JOIN cities on cities.state_id = states.id\
                 WHERE states.name = %s\
                 ORDER BY cities.id ASC;"
-        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+        conn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                charset="utf8")
         cur = conn.cursor()
         cur.execute(cmd, (state_name,))
         query_rows = cur.fetchall()
