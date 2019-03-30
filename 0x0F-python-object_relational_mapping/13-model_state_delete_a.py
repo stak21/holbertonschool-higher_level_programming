320002bdedaff2ad6b3dfd54a0a3b13453dee4b3
stak21 <stakashima21@gmail.com>
2019-03-30 06:06:15 +0000

    Initial Commit: Deletes all states that contain an a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script: lists all State objects that contain the letter """
+from model_state import State, Base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        usr_name = sys.argv[1]
+        usr_pass = sys.argv[2]
+        usr_db = sys.argv[3]
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                               format(usr_name, usr_pass, usr_db))
+        Session = sessionmaker(bind=engine)
+
+        session = Session()
+        try:
+            for state in session.query(State).\
+                    filter(State.name.ilike('%a%')).\
+                    all():
+                session.delete(state)
+        except:
+            pass
+        session.commit()
+        session.close()
+    else:
+        print("my sql username pass db ")
