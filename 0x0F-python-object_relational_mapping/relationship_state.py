2222a55c0c011c0f143ff1b109b1fee5936a124e
stak21 <stakashima21@gmail.com>
2019-03-30 20:27:04 +0000

    Added a relationship to City cascading

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.4
+""" Class: State - Base Model of a state """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class: A Class to map to a table """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all,delete", backref="state")
+
