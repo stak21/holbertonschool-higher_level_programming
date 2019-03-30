96acb8de9acf9da6529da8b3df851bd3bfca4f50
stak21 <stakashima21@gmail.com>
2019-03-30 05:45:02 +0000

    Initial Commit: Script: Adds a state Louisiana to the Database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Script: lists all State objects that contain the letter """
+from model_state import State, Base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        usr_name = sys.argv[1]
+        usr_pass = sys.argv[2]
+        usr_db = sys.argv[3]
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                               format(usr_name, usr_pass, usr_db))
+        Session = sessionmaker(bind=engine)
+
+        session = Session()
+        new_state = State(name="Louisiana")
+        session.add(new_state)
+        session.commit()
+        for state in session.query(State).\
+                order_by(State.id).\
+                all():
+                    print("{}: {}".format(state.id, state.name))
+        session.close()
+    else:
+        print("my sql username pass db ")
