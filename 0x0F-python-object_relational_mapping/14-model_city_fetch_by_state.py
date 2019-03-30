3d2fd23b0ccf77eb08b2cce75148d37089a4e400
stak21 <stakashima21@gmail.com>
2019-03-30 07:26:41 +0000

    Initial Commit: Lists all cities with their state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Script: lists all city objects """
+from model_state import State, Base
+from model_city import City, Base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        usr_name = sys.argv[1]
+        usr_pass = sys.argv[2]
+        usr_db = sys.argv[3]
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                               format(usr_name, usr_pass, usr_db))
+        Session = sessionmaker(bind=engine)
+
+        session = Session()
+        city = session.query(City).\
+                order_by(City.id).\
+                all()
+        author =  city.states
+        print("{}: ({}) {}".format(author.name, city.id, city.name))
+        session.close()
+    else:
+        print("my sql username pass db ")
