260560b1b63705b7dd24917f25a723f11fb42195
stak21 <stakashima21@gmail.com>
2019-03-30 03:38:30 +0000

    Initial Commit: Base model for State Class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.4
+""" Class: State - Base Model of a state """
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
