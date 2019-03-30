be61dc63f5ee031c0f737fc0edc55c9b41abcf2b
stak21 <stakashima21@gmail.com>
2019-03-30 19:38:06 +0000

    Changed connection frmo cities.id to states.id; removed declarative base(Must use the same base as the other tableclasses)

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,11 +2,7 @@
 """ Class: cities - Base Model of a City """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import State, Base
-
-
-Base = declarative_base()
 
 
 class City(Base):
@@ -15,6 +11,6 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('cities.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
-    state = relationship("State", backref="cities")
+    state = relationship("State", backref="children")
