975f4cb1b2bcd787a09f53e9ad31c809bc47e5a4
stak21 <stakashima21@gmail.com>
2019-03-30 20:36:01 +0000

    Removed extra line

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,4 +15,3 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all,delete", backref="state")
-
