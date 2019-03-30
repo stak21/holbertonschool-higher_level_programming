7cdea8ad915ec19317943428c493ac63791df6a0
stak21 <stakashima21@gmail.com>
2019-03-30 03:43:38 +0000

    pep8 compliant

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,9 +4,13 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
+
 class State(Base):
+    """ Class: A Class to map to a table """
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
