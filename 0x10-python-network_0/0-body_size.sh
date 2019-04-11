121cbbcd5ed7a3253c10ff33fb1b7651c1521827
stak21 <stakashima21@gmail.com>
2019-04-11 02:24:30 +0000

    changed shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Bash script to send a request to the url and display the body size of the
-curl -w "%{size_download}"\\n -o /dev/null -s $1
+curl -w "%{size_download}"\\n -o /dev/null -s "$1"
