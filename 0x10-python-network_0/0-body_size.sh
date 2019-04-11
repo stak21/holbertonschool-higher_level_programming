3eeaeb7f4d199edb8bce577e78f7d0c3aa6f8b57
stak21 <stakashima21@gmail.com>
2019-04-11 01:40:03 +0000

    Removed the ipaddress

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Bash script to send a request to the url and display the body size of the
 # response
 
-curl -sI 172.31.54.208:42887 | awk '/Content-Length/ { print $2 }'
+curl -sI "$1" | awk '/Content-Length/ { print $2 }'
