58d56c699a9ef8e949b546a3623bebfd87ef7222
stak21 <stakashima21@gmail.com>
2019-04-16 08:10:57 +0000

    Initial Commit: send a search request

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -6,4 +6,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.get("https://swapi.co/api/people/?search={}", sys.argv[1])
+    r =\
+    requests.get("https://swapi.co/api/people/?search={}".format(sys.argv[1]))
+    dic = r.json()
+    print("Number of results: {}".format(dic['count']))
+    for res in dic['results']:
+        print("{}".format(res['name']))
