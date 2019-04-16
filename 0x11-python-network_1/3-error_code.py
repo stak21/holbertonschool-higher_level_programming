6496548a02854a6ed885f05f0595864d85c1403a
stak21 <stakashima21@gmail.com>
2019-04-16 06:09:20 +0000

    dded import_guard

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,9 +3,10 @@ import urllib.request
 import urllib.error
 import sys
 
-req = urllib.request.Request(sys.argv[1])
-try:
-        with urllib.request.urlopen(req) as response:
-                print(response.read().decode("utf-8"))
-except urllib.error.URLError as e:
-        print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+            with urllib.request.urlopen(req) as response:
+                    print(response.read().decode("utf-8"))
+    except urllib.error.URLError as e:
+            print("Error code: {}".format(e.code))
