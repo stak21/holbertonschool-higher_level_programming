bd8e59cdfc5d8956752bc3c5046c72b197c74823
stak21 <stakashima21@gmail.com>
2019-04-16 05:25:30 +0000

    dded shebang and a import_guard

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python3
+"""Script: Given a url and a email, send a POST request
+"""
 import urllib.request
 import urllib.parse
 import sys
 
-values = {'email':sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as response:
-        print("Your email is: {}".format(response.read().decode("utf-8")))
+if __name__ == "__main__":
+    values = {'email':sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+            print("Your email is: {}".format(response.read().decode("utf-8")))
