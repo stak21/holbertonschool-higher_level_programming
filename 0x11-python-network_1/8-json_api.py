2343d9cfe2d47ffa3588d1b624b3837feefefe66
stak21 <stakashima21@gmail.com>
2019-04-16 07:37:24 +0000

    Added a argv validation

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,13 +8,13 @@ import sys
 
 if __name__ == "__main__":
     params = {'q': ""}
-    if sys.argv[1]:
+    if len(sys.argv) == 2 and sys.argv[1]:
         params['q'] = sys.argv[1]
     r = requests.post('http://0.0.0.0:5000/search_user', params)
     try:
         if not r.json():
             print("No result")
         else:
-            print("[{}] {}hi".format(r.json()['id'], r.json()['name']))
+            print("[{}] {}".format(r.json()['id'], r.json()['name']))
     except:
         print("Not a valid JSON")
