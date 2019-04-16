18f857a570f1853ef62c469cbdd9c8c53175bef1
stak21 <stakashima21@gmail.com>
2019-04-16 06:32:06 +0000

    Initial Commit: Print the header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Script: given a URL, send a request display a header
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['x-request-id'])
