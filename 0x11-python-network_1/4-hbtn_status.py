46c6a6062f71f4f27b9cebef2a0b61e0a82317c6
stak21 <stakashima21@gmail.com>
2019-04-16 06:24:06 +0000

    removed extra spaces

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Script: fetch a website using requests
+"""
+Script: fetch a website using requests
 """
 import requests
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}".format(type(r.text), r.text))
+\t- type: {}\n\
+\t- content: {}".format(type(r.text), r.text))
