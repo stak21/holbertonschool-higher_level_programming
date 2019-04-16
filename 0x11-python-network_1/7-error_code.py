c4e8b9bf5120b48d8e0f586fc36da5c0589e90a1
stak21 <stakashima21@gmail.com>
2019-04-16 06:52:44 +0000

    Added Import_guard

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,10 +5,9 @@ Script: send a request and display the error codes
 import requests
 import sys
 
-
-r = requests.get(sys.argv[1])
-if r.status_code >= 400:
-    print("Error code: {}".format(r.status_code))
-else:
-    print(r.text)
-            
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
