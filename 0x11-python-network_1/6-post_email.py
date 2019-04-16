e50d3560c6017f8bb53dd3357816f1ebf49ce1a5
stak21 <stakashima21@gmail.com>
2019-04-16 06:46:01 +0000

    added quotes around email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    values = {email: sys.argv[2]}
+    values = {'email': sys.argv[2]}
     r = requests.post(sys.argv[1], values)
     print("{}".format(r.text))
