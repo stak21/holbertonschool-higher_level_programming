1f8ec17e43c9b373ab0bf2b2149f23a65bfdce64
stak21 <stakashima21@gmail.com>
2019-04-16 07:58:27 +0000

    Removed whitespaces and added import_guard

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Given a string, send a serach request 
+Given a string, send a serach request
 """
 import requests
 import sys
 
-
-r = requests.get("https://swapi.co/api/people/?search={}", sys.argv[1])
+if __name__ == "__main__":
+    r = requests.get("https://swapi.co/api/people/?search={}", sys.argv[1])
