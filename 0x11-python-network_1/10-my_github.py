87f6dac332058918be1b708a628b599b03d11175
stak21 <stakashima21@gmail.com>
2019-04-19 02:11:10 +0000

    Initial Commit: use HTTPBasicAuth

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Send a request to github with a user and password
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if len(sys.argv) == 3:
+    params = sys.argv[1]
+    r = requests.get("https://api.github.com/user", auth=HTTPBasicAuth(
+        sys.argv[1], sys.argv[2]))
+    try:
+        print(r.json()['id'])
+    except:
+        print("None")
+
+
