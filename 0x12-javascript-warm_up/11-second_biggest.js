763b6a89c4363a008c08ab10e25444227e29765b
stak21 <stakashima21@gmail.com>
2019-05-08 05:22:29 +0000

    fix: dup numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,6 +7,9 @@ const li = process.argv;
 let max = li[2];
 let low = li[3];
 for (let i = 3; i < process.argv.length; i += 1) {
+  if (Number(li[i]) === Number(max)) {
+    li.splice(i);
+  }
   if (Number(li[i]) > Number(max)) {
     low = max;
     max = li[i];
