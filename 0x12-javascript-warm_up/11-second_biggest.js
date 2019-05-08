bfc08729b616ea351696d5311d2c828a1a9d487b
stak21 <stakashima21@gmail.com>
2019-05-08 04:02:12 +0000

    fix: set low to the next argument

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ if (process.argv.length <= 3) {
 }
 const li = process.argv;
 let max = li[2];
-let low = max;
+let low = li[3];
 for (let i = 3; i < process.argv.length; i += 1) {
   if (Number(li[i]) > Number(max)) {
     low = max;
