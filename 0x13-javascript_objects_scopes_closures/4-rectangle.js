43675eb7ab2f387b62d11c35d336e98a5c4fd5ae
stak21 <stakashima21@gmail.com>
2019-05-14 06:03:45 +0000

    feat(0x13 higher_programming): Build on the previous task, add the methods Double and Rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w = null, h = null) {
+    if (h <= 0 || w <= 0) {
+      return null;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i += 1) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    let temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+    
+};
