75e9427788c67d6ed566271a935b2f5261b73c3d
stak21 <stakashima21@gmail.com>
2019-05-14 08:02:17 +0000

    fix(0x13 higher_programming): wasn't including the last insertion, >= 0

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = (list) => {
-  if (list == null || li
-  let length = list.length;
+  if (list == null) { return 0; }
+  let length = list.length - 1;
   let newList = [];
-  for (length; length > 0; length -= 1) {
+  for (length; length >= 0; length -= 1) {
     newList.push(list[length]);
   }
   return newList;
