2cc8f7ad464b37fde3bdbf604cc0b31c76ee358e
stak21 <stakashima21@gmail.com>
2019-05-14 06:29:04 +0000

    fix(0x13 higher_programming): fix semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,14 +4,13 @@ const Rectangle = require('./5-square');
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
   charPrint (c) {
     if (c == null) {
       super.print();
     } else {
-      for (let i = 0; i < this.size; i += 1) {
-        console.log(c.repeat(this.size));
+      for (let i = 0; i < this.height; i += 1) {
+        console.log(c.repeat(this.width));
       }
     }
   }
