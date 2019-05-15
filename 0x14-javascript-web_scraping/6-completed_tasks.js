197aa2b04dc39d0f88244b875e88d7a9b48ddce7
stak21 <stakashima21@gmail.com>
2019-05-15 04:31:48 +0000

    fix(0x14 higher_programming): added else to the error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,18 +4,19 @@ const request = require('request');
 const url = process.argv[2];
 
 let users = {};
-request.get(url, function (body, err, response) {
-  if (err) {
-    console.log(err);
+request.get(url, function (error, body, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    let tasks = JSON.parse(response);
+    tasks.forEach(task => {
+      if (users[task['userId']] === undefined) {
+        users[task['userId']] = 0;
+      }
+      if (task['completed'] === true) {
+        users[task['userId']] += 1;
+      }
+    });
+    console.log(users);
   }
-  let tasks = JSON.parse(response);
-  tasks.forEach(task => {
-    if (users[task['userId']] === undefined) {
-      users[task['userId']] = 0;
-    }
-    if (task['completed'] === true) {
-      users[task['userId']] += 1;
-    }
-  });
-  console.log(users);
 });
