0e992dec3c8a4770acaff78934e247ff91e24329
stak21 <stakashima21@gmail.com>
2019-05-15 04:38:22 +0000

    changed all responses to body

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,11 +5,11 @@ const request = require('request');
 let users = {};
 if (process.argv.length >= 3) {
   const url = process.argv[2];
-  request.get(url, function (error, body, response) {
+  request.get(url, function (error, response, body) {
     if (error) {
       console.log(error);
     } else {
-      let tasks = JSON.parse(response);
+      let tasks = JSON.parse(body);
       tasks.forEach(task => {
         if (users[task['userId']] === undefined) {
           users[task['userId']] = 0;
