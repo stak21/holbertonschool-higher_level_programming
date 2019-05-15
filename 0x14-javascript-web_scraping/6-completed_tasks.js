55d2da7ee8a77bd06aee650eaee633d9168537ba
stak21 <stakashima21@gmail.com>
2019-05-15 04:33:10 +0000

    fix(0x13 higher_programming): added arg length validatio

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
 
 let users = {};
-request.get(url, function (error, body, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    let tasks = JSON.parse(response);
-    tasks.forEach(task => {
-      if (users[task['userId']] === undefined) {
-        users[task['userId']] = 0;
-      }
-      if (task['completed'] === true) {
-        users[task['userId']] += 1;
-      }
-    });
-    console.log(users);
-  }
-});
+if (process.argv.length >= 3) {
+  const url = process.argv[2];
+  request.get(url, function (error, body, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      let tasks = JSON.parse(response);
+      tasks.forEach(task => {
+        if (users[task['userId']] === undefined) {
+          users[task['userId']] = 0;
+        }
+        if (task['completed'] === true) {
+          users[task['userId']] += 1;
+        }
+      });
+      console.log(users);
+    }
+  });
+}
