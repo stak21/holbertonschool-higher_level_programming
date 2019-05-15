eee07f9ca1c1483ed4258c074b7c8fc25d10b4d4
stak21 <stakashima21@gmail.com>
2019-05-15 04:17:21 +0000

    fix(0x14 higher_programming): err catch

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,6 +5,9 @@ const url = process.argv[2];
 
 let users = {};
 request.get(url, function (body, err, response) {
+  if (err) {
+    console.log(err);
+  }
   let tasks = JSON.parse(response);
   tasks.forEach(task => {
     if (users[task['userId']] === undefined) {
