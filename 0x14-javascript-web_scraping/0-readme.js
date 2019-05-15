880d159824bd73cd00382c07dd8195b13234437d
stak21 <stakashima21@gmail.com>
2019-05-15 01:12:40 +0000

    fix(0x14 higher_programming): semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-let fs = require("fs");
+let fs = require('fs');
 
 let path = './' + process.argv[2];
 fs.readFile(path, 'utf8', function (err, data) {
-  if (err){
-    console.log(err)
+  if (err) {
+    console.log(err);
   } else {
-    console.log(data)
+    console.log(data);
   }
 });
